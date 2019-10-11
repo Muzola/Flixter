6fbe53c5f079d32c4ff2b87b590d8217eba81bea
Muzola <amazzolaphoto@gmail.com>
2019-10-11 18:52:16 +0000

    trying CW again

--- config/initializers/carruerwave.rb
+++ config/initializers/carruerwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIAU6NCY5BNCKPJX7TZ"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["anthony-m-flixter"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
